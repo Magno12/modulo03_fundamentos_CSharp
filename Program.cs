75596d8cae507f7e83310c8d1e7a10e948ce5f92
Magno batista <magnobatista75@gmail.com>
2022-05-21 16:51:41 -0300

    aula demo reference types na pratica

--- Program.cs
+++ Program.cs
@@ -4,17 +4,43 @@ using static System.Console;
 public class Program
 {
 
-    static int Adicionar20(int x)
+
+    //aula 04 values types
+    /*  static int Adicionar20(int x)
+     {
+         return x = x + 20;
+     }
+     public static void Main()
+     {
+         int a = 2;
+         Adicionar20(a);
+         a = Adicionar20(a);
+
+         WriteLine($" O valor da variável A e : {a} "); // ao importa o System.Console podemos chamar direto o WriteLine
+     } */
+
+    //Aula 05 reference types na pratica
+
+    public static void _AlterarNome(Pessoa pessoa, string nome)
     {
-        return x = x + 20;
+        pessoa.Nome = nome;
     }
+
     public static void Main()
     {
-        int a = 2;
-        Adicionar20(a);
-        a = Adicionar20(a);
+        Pessoa p1 = new Pessoa();
+        p1.Nome = "Magno";
+        p1.Idade = 27;
+        p1.Documento = "1234";
 
-        WriteLine($" O valor da variável A e : {a} "); // ao importa o System.Console podemos chamar direto o WriteLine
+        WriteLine("Dados Iniciais");
+        WriteLine("Nome: " + p1.Nome + " Idade: " + p1.Idade + " Documento: " + p1.Documento);
+        WriteLine("-----");
+
+        _AlterarNome(p1, "Magno Batista Rocha");
+        WriteLine("Alterando Nome");
+        WriteLine($"Nome: {p1.Nome}, Idade: {p1.Idade}, Documento ${p1.Documento} ");
+
     }
 
-}
+}   
