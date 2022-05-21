957b01ac4010d5f873fadc2bbcc5a62739b32587
Magno batista <magnobatista75@gmail.com>
2022-05-21 16:11:17 -0300

    demo value types na pratica

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
-﻿
+﻿//Value Types na prática
+
+using static System.Console;
 public class Program
 {
+
+    static int Adicionar20(int x)
+    {
+        return x = x + 20;
+    }
     public static void Main()
     {
-        Console.WriteLine("Hello World");
+        int a = 2;
+        Adicionar20(a);
+        a = Adicionar20(a);
+
+        WriteLine($" O valor da variável A e : {a} "); // ao importa o System.Console podemos chamar direto o WriteLine
     }
 
 }
